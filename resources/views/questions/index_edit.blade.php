ab4432a4fdd3e4d041eb034fffe66544b1747015
miaobaobao <joychoinjapan@gmail.com>
2020-06-23 02:53:06 +0900

    操作ブロックの追加

--- resources/views/questions/index_edit.blade.php
+++ resources/views/questions/index_edit.blade.php
@@ -11,13 +11,23 @@
             <div class="col-md-4 col-md-offset-2">
                 <div class="card mb-2">
                     <div class="card-header-title">
-                       操作ブロック
+                       操作
                     </div>
-                    <div class="card-content">
-                        <p>プログラミングって独学でやると難しいでしゃうか</p>
-                        <p>プログラミングって独学でやると難しいでしゃうか</p>
-                        <p>プログラミングって独学でやると難しいでしゃうか</p>
-                        <p>プログラミングって独学でやると難しいでしゃうか</p>
+                    <div class="card-content p-3">
+                        <div class="columns has-text-centered is-centered">
+                            <div class="column">
+                                <i class="fas fa-chalkboard-teacher fa-2x m-1"></i>
+                                <p>答える</p>
+                            </div>
+                            <div class="column">
+                                <i class="far fa-clipboard fa-2x m-1"></i>
+                                <p>ノート</p>
+                            </div>
+                            <div class="column">
+                                <i class="far fa-folder fa-2x m-1"></i>
+                                <p>ストック</p>
+                            </div>
+                    </div>
                     </div>
                 </div>
                 <div class="card mb-2">
